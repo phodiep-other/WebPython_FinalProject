f83d89b0dd75061e3070bfa6138f8fa82afd41eb
Pho Diep <phodiep@gmail.com>
2013-03-12 19:47:26 -0700

    commit hello clapp

--- CLapp.py
+++ CLapp.py
@@ -13,27 +13,25 @@ def search_form():
 
 @app.route('/search', method='POST')
 def search_submit():
-    location = request.forms.get('location')
-    bed = request.forms.get('bed')
-    rentmin = request.forms.get('rentmin')
-    rentmax = request.forms.get('rentmax')
-    
-    results = CLSearch.get_list(location,rentmin,rentmax,bed)
-    return template('search_results.tpl',
-                    location=location,
-                    bed=bed,
-                    rentmin=rentmin,
-                    rentmax=rentmax,
-                    results=results)
+##    location = request.forms.get('location')
+##    bed = request.forms.get('bed')
+##    rentmin = request.forms.get('rentmin')
+##    rentmax = request.forms.get('rentmax')
+##    
+##    results = CLSearch.get_list(location,rentmin,rentmax,bed)
+##    return template('search_results.tpl',
+##                    location=location,
+##                    bed=bed,
+##                    rentmin=rentmin,
+##                    rentmax=rentmax,
+##                    results=results)
+    return 'hello'
 
 
 @app.error(404)
 def error404(error):
     return "sorry, doesn't exist"   
 
-##if __name__ == '__main__':
-##    port = os.environ.get('PORT',8080)
-##    run(app, host='0.0.0.0', port=port)
-
-port = os.environ.get('PORT',8080)
-run(app, host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = os.environ.get('PORT',8080)
+    run(app, host='0.0.0.0', port=port)
