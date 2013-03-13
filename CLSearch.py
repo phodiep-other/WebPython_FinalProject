837eae8412d3784ae5333a2947142055d6274643
Pho Diep <phodiep@gmail.com>
2013-03-12 20:41:23 -0700

    commit

--- CLSearch.py
+++ CLSearch.py
@@ -106,7 +106,7 @@ def parse_posts(posts):
 def get_list(query,rentmin,rentmax,rooms):
   resultList = []
   url = get_url(query,rentmin,rentmax,rooms)
-##  parsed = parse_url(url)
+  parsed = parse_url(url)
 ##  posts = parsed.find_all('p', class_='row')
 ##  resultList = parse_posts(posts)
   return resultList
